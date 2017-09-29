bc3688c945afeaa7d3f12656b2c4309aea1232d8
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-09-29 16:52:44 -0300

    adding the reducer that is going to handle the actions

---
+++ reducers/TwitchApp.js
@@ -0,0 +1,34 @@
+//define the initial state
+const initialState = {
+    status: '',
+    stream: [],
+    error: ''
+};
+
+
+//define a reducer with an initialized state and logic to handle action
+function TwitchApp(state = initialState, action) {
+    switch(action.type) {
+        case 'FETCH_REQUEST':
+            const requested = Object.assign({}, state, {
+                status: action.status
+            });
+            return requested;
+        case 'FETCH_SUCCESS':
+            const successful = Object.assign({}, state, {
+                status: action.status,
+                streams: action.streams
+            });
+            return successful;
+        case 'FETCH_FAILURE':
+            const failed = Object.assign({}, state, {
+                status: action.status,
+                error: action.error
+            });
+            return failed;
+        default:
+            return state
+    }
+}
+
+export default TwitchApp
